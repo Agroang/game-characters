2f69947a45e86816aa25392a77529855248cd4e3
matias acuna <agroang@gmail.com>
2021-11-14 07:31:08 +0900

    Add dotenv - Protect my secret data in .env file

--- app/controllers/gains_controller.rb
+++ app/controllers/gains_controller.rb
@@ -1,10 +1,32 @@
 class GainsController < ApplicationController
   def new
+    @character = Character.find(params[:character_id])
+    @gain = Gain.new
   end
 
   def create
+    @character = Character.find(params[:character_id])
+    # @skill = Skill.find(params[:skill_id])
+    @gain = Gain.new(gain_params)
+    # @gain.skill = @skill
+    @gain.character = @character
+    @game = Game.find(@gain.character.game.id)
+    if @gain.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @gain = Gain.find(params[:id])
+    @gain.destroy
+    redirect_to game_path(@gain.character.game)
+  end
+
+  private
+
+  def gain_params
+    params.require(:gain).permit(:character_id, :skill_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,22 @@
-puts "Trying to destroy current data...."
+puts "Destroying current data...."
 
 Game.destroy_all
 Skill.destroy_all
 
-puts "Trying to Create 1 instance of each"
+puts "Creating 10 skills..."
 
-wow = Game.create!(name: "World of Warcraft")
-sylvanas = Character.create!(name: "Sylvanas", game_id: wow.id)
+# wow = Game.create!(name: "World of Warcraft")
+# sylvanas = Character.create!(name: "Sylvanas", game_id: wow.id)
 thunder = Skill.create!(name: "Thunder")
-Gain.create!(character_id: sylvanas.id, skill_id: thunder.id)
+fire = Skill.create!(name: "Fire Laser")
+ice = Skill.create!(name: "Ice Storm")
+earthquake = Skill.create!(name: "Earthquake")
+heal = Skill.create!(name: "Heal")
+flash = Skill.create!(name: "Flash")
+explosion = Skill.create!(name: "Explosion")
+poison = Skill.create!(name: "Poison Darts")
+cure = Skill.create!(name: "Cure")
+invisibility = Skill.create!(name: "Invisibility")
+# Gain.create!(character_id: sylvanas.id, skill_id: thunder.id)
 
-puts "1 Game, Character, Skill and Gain were created ✨"
+puts "Skills created! ✨"
